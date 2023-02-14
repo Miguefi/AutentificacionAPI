27a07dc8d9bcfa941c0aa4af085007b05cb64b16
Miguefi <martinezmiguel417@gmail.com>
2023-02-14 21:50:08 +0100

    Initial Commit 2

--- app/src/main/java/com/example/autentificacionapi/LoginViewModel.java
+++ app/src/main/java/com/example/autentificacionapi/LoginViewModel.java
@@ -6,8 +6,12 @@ import androidx.lifecycle.ViewModel;
 
 import com.example.autentificacionapi.api.QuestionRepository;
 
+import java.util.ArrayList;
+
 public class LoginViewModel extends ViewModel {
 
+    private static final double TEST_WAIT = 2000;
+    private static final double SUCESS_PROBABILITY = 50;
     private MutableLiveData<LoginResponse> mlData;
     private QuestionRepository questionRepository;
 
@@ -20,6 +24,22 @@ public class LoginViewModel extends ViewModel {
     }
 
     public void hacerLogin(String username , String password) {
-
+        new Thread(() -> {
+            try {
+                Thread.sleep((long) (Math.random()*TEST_WAIT));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            LoginResponse response = new LoginResponse();
+            if (Math.random()>SUCESS_PROBABILITY) {
+                response.setToken("LOHASCONSEGUIDOCAMPEON");
+                response.setNonFieldErrors(new ArrayList<String>());
+            } else {
+                response.setToken("");
+                response.setNonFieldErrors(new ArrayList<String>());
+                response.getNonFieldErrors().add("Ha ocurrido un error");
+            }
+            mlData.postValue(response);
+        }).start();
     }
 }
